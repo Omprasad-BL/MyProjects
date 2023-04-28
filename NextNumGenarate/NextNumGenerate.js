b0d7f84fc45f641e584730fec26ab428c52f3c1b
Omprasadbl <omprasadbl@gmail.com>
2023-04-28 11:45:00 +0530

    Completed the Assessment For Internship

--- NextNumGenarate/NextNumGenerate.js
+++ NextNumGenarate/NextNumGenerate.js
@@ -2,32 +2,37 @@ const numInput = document.getElementById("numInput");
 
 numInput.addEventListener("keyup", function () {
   let num = parseInt(numInput.value);
+
   console.log(typeof num);
   console.log(num);
+if (isNaN(num)) {
+   document.getElementById("next").innerHTML="";
+  // if value is NaN means empty after removing digit it will clear last remaining digits next three values
+}    //end of if block    isNAN()
 
   if (num < 0) {
     window.alert("enter positive number");
     numInput.value = "";
     numInput.focus();
   } 
-  else if (num > 0) {
+  else if (num > -1) {
     if (num % 2 == 0) {
-      console.log("Positive");
-      //   if (num%2!=0) {
-      // var count=3;
-      // while (count!=0) {
-
+      // even number block
+      console.log("Even Number");
       var nextval = document.getElementById("next");
       nextval.innerHTML = `${Number(num + 2)}  ${num + 4} ${num + 6}`;
-      //     }
-      //   }
     } else {
-      console.log("Negetive");
+      //  odd number block
+      console.log("Odd Number");
       var nextval = document.getElementById("next");
       nextval.innerHTML = `${Number(num + 2)}  ${num + 4} ${num + 6}`;
-    }
-  }
-  else if(num==NaN){
+    
+    } 
+  }  // end of else if block
+
+else{
+  nextval.value="";
   nextval.innerHTML="";
-  }
-});
+}
+ 
+}); // end of event listener
