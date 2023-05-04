c32b51e287229f652e4b475788036679cd8fa63f
Omprasadbl <omprasadbl@gmail.com>
2023-05-04 23:57:37 +0530

    Emoji Gnerator Update

--- EmojiGenerator/Emoji.js
+++ EmojiGenerator/Emoji.js
@@ -1,45 +1,37 @@
-
-// function random() {
-    //  let emj= Math.floor(Math.random()*10);
-    // var emjArea=document.getElementById("val");
-
-    // console.log("Hello work ");  REFERENCE COPY
-    // let emj=Math.floor(100000 + Math.random() * 900000);
-    // emjArea.innerHTML=emj;
-
-    
-    // console.log("Hello work ");
-    // let emj=Math.floor((8986) + Math.floor(Math.random() * 129510));
-    // emjArea.innerHTML= `&#${emj}`;
-
-    // };
-let code="";
-console.log(Math.floor(Math.random()*9000));
-for(let i=1;i<5;i++){
-    code+=Math.floor(Math.random()*9);
+function random() {
+  //  let emj= Math.floor(Math.random()*10);
+  var emjArea = document.getElementById("val");
+  console.log("Hello work ");
+  // let emj=Math.floor((8986) + Math.floor(Math.random() * 129510));
+  var emj =0;
+  while (emj < 8986) {
+    emj = Math.floor(Math.random() * 129510);
+  }
+  emjArea.innerHTML = `&#${emj}`;
+  document.getElementById("code").innerHTML=emjArea.textContent;
 
 }
-console.log(code);
-    // console.log(
-    //     Math.floor(Math.random() * 1000000)
-    //     );
-    
-    // var number = Math.floor(Math.random() * 9000000000) + 1000000000;
-    // console.log(number);
-
-    // function random(len) {
-    //     let result = Math.floor(Math.random() * Math.pow(10, len));
-      
-    //     return (result.toString().length < len) ? random(len) : result;
-    //   }
-      
-    //   console.log(random(6));
 
 
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-      }
-console.log( getRandomInt(8986,129510));      
-console.log(Math.floor(Math.random()*(129510-8986) + 129510 ));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(emj=Math.floor(Math.random()*(129510)));
+// let str="";
+// for(let a=1;a<5;a++){
+//     str+=Math.floor(Math.random()*16).toString(16)
+//     console.log(str);
+
+// }
---
+++ check.js
@@ -0,0 +1,14 @@
+
+function getRandom(min, max) {
+  const floatRandom = Math.random()
+
+  const difference = max - min
+
+  // random between 0 and the difference
+  const random = Math.round(difference * floatRandom)
+
+  const randomWithinRange = random + min
+
+  return randomWithinRange
+}
+console.log(getRandom(8986,129512));
