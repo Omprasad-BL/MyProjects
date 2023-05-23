ad25365d06dcf6559dcafd98e8ff06c91fd3e679
Omprasadbl <omprasadbl@gmail.com>
2023-05-23 23:48:57 +0530

    javascript storages

---
+++ Stotages/storage.js
@@ -0,0 +1,19 @@
+let storages=localStorage.setItem("names","omprasad B L");
+let names=localStorage.setItem("age","omprasresvead B L");
+let adr=localStorage.setItem("adr","omprasaerfvsrsfd B L");
+let hob=localStorage.setItem("hob","ompdeffdfd");
+console.log(localStorage.getItem("names"));
+console.log(localStorage.key("names"));
+console.log(localStorage.removeItem("names"));
+console.log(localStorage.getItem("names"));
+console.log(localStorage.length);
+console.log(localStorage.key("age"));
+
+
+
+let sess1=sessionStorage.setItem("names","omprasad");
+let age=sessionStorage.setItem("age","22");
+let adrr=sessionStorage.setItem("adr","shivamoga ")
+console.log(sessionStorage.getItem("adrr"));
+console.log(sessionStorage.length);
+console.log(sessionStorage.removeItem("age"));
