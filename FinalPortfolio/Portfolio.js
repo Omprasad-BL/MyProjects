fb7976b9d4d9befce575b9a49553130a807c81c0
Omprasadbl <omprasadbl@gmail.com>
2023-06-10 11:08:01 +0530

    MY Portfolio responsive

---
+++ FinalPortfolio/Portfolio.js
@@ -0,0 +1,10 @@
+
+function w3_open() {
+    document.getElementsByClassName("w3-sidenav")[0].style.display = "grid";
+    document.getElementsByClassName("w3-opennav")[0].style.display="none";
+}
+function w3_close() {
+    document.getElementsByClassName("w3-sidenav")[0].style.display = "none";
+    document.getElementsByClassName("w3-opennav")[0].style.display="block";
+
+}   
