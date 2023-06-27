64ed54b17a1271ea119aa14d885da3f76b10c185
Omprasadbl <omprasadbl@gmail.com>
2023-06-27 22:33:58 +0530

    Async Ajax and Fetch api Practice done

---
+++ AsynchAwait/Asynch.js
@@ -0,0 +1,86 @@
+// async function Demo(ml) {
+//     var val= new Promise((resolve, reject)=>setTimeout(resolve,ml));
+//     resolve("Hello resolved")
+//     return val;
+// }
+// Demo(2000);
+
+
+function prom(timeout) {
+    var promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("hello Issue resolved");
+
+        }, timeout);
+        reject("issue not resolved ");
+    });
+
+    //   Promise work
+    promise.then((result) => {
+        console.log(result);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+prom(3000);
+
+
+//  using async await  and  Fetch
+async function Demo2() {
+    var dataa = await fetch("https://dummyjson.com/users");
+
+    var ans = await dataa.json();
+    // console.log( typeof ans);
+
+    console.log(ans.users[5].image);
+
+    // For each for all elements stored in form of array
+    ans.users.forEach(element => {
+        console.log(element.firstName.trim(), "\t \t \t", element.lastName.trim(), "\t \t", element.birthDate)
+    });
+
+}
+Demo2();
+
+
+//  Fetch api
+function fetchText() {
+    fetch('https://dummyjson.com/users')
+        .then(response => response.json())
+        // .then(data => data.users.map((element) => {
+        // console.log(element.firstName);
+        .then((element) => {
+            console.log(element);
+            let table = "<tr><th>fname</th><th>lname</th><th> address</th> <th> phone</th> <th> email</th></tr>";
+            element.users.forEach(element => {
+        // console.log(element.firstName.trim(), "\t \t \t", element.lastName.trim(), "\t \t", element.birthDate)
+
+                table += "<tr><td>";
+                table += element.firstName;
+                table += "</td><td>";
+                table += element.lastName;
+                table += "</td><td>";
+                table += element.address.address;
+                table += "</td><td>";
+                table += element.phone;
+                table += "</td><td>";
+                table += element.email;
+                table += "</td></tr>";
+    });
+               
+            
+            document.getElementById("tbl").innerHTML = table;
+        })
+
+        // }))
+        .catch((err) => {
+            console.log(err);
+        })
+
+
+}
+fetchText();
+
+
+
+
